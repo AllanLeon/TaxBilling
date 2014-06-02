b9fd2ffd2386f66589f336f15784ba210c308b71
Allan Leon <allan.leon@outlook.com>
2014-06-01 23:49:44 -0400

    Made BillRow class extends TableRow instead of Fragment

--- src/com/upb/taxbilling/view/BillRow.java
+++ src/com/upb/taxbilling/view/BillRow.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.LayoutInflater;
@@ -20,56 +21,13 @@ import com.upb.taxbilling.model.data.Bill;
  * The fragment where a bill row is stored.
  * @author Allan Leon
  */
-public class BillRowFragment extends Fragment {
+public class BillRow extends TableRow {
 
-	private static String KEY_ARG_NIT = "nit";
-	private static String KEY_ARG_BILL_NUMBER= "billNumber";
-	private static String KEY_ARG_AUTORIZATION_NUMBER = "autorizationNumber";
-	private static String KEY_ARG_DATE = "date";
-	private static String KEY_ARG_AMOUNT = "amount";
-	private static String KEY_ARG_CONTROL_CODE = "controlCode";
-	
 	Bill bill;
 	
-	/**
-     * {@inheritDoc}
-     */
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		TableLayout table = (TableLayout) inflater.inflate(R.id.ContentTable,
-				container, false);
-		TableRow row = new TableRow(table.getContext());
-		addViewsToRow(row);
-	    return row;
-	}
-
-	/**
-	 * Creates a new instance of a bill row fragment with a given bill and adds the
-	 * given bill data to the bundle arguments.
-	 * @param bill
-	 * @return
-	 */
-	public static BillRowFragment newInstance(Bill bill) {
-		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-		BillRowFragment brf = new BillRowFragment();
-	    brf.setBill(bill);
-
-	    // Get arguments passed in, if any
-	    Bundle args = brf.getArguments();
-	    if (args == null) {
-	        args = new Bundle();
-	    }
-	    // Add parameters to the argument bundle
-	    args.putInt(KEY_ARG_NIT, bill.getNit());
-	    args.putInt(KEY_ARG_BILL_NUMBER, bill.getBillNumber());
-	    args.putInt(KEY_ARG_AUTORIZATION_NUMBER, bill.getAutorizationNumber());
-	    args.putString(KEY_ARG_DATE, df.format(bill.getEmissionDate()));
-	    args.putDouble(KEY_ARG_AMOUNT, bill.getAmount());
-	    args.putString(KEY_ARG_CONTROL_CODE, bill.getControlCode());
-	    brf.setArguments(args);
-
-	    return brf;
+	public BillRow(Context context) {
+		super(context);
+		addViewsToRow(this);
 	}
 
 	/**
@@ -94,7 +52,8 @@ public class BillRowFragment extends Fragment {
 	private void addViewsToRow(TableRow row) {
 		EditText t1 = new EditText(row.getContext());
     	t1.setInputType(InputType.TYPE_CLASS_NUMBER);
-    	t1.setText(Integer.toString(getNextBillNumber()));
+    	//t1.setText(Integer.toString(getNextBillNumber()));
+    	t1.setText("0");
     	t1.setEnabled(false);
     	
     	EditText t2 = new EditText(row.getContext());
@@ -131,24 +90,6 @@ public class BillRowFragment extends Fragment {
     	
     	updateRowInfo(row);
 	}
-	
-	/**
-     * Returns the next bill number of the bill table.
-     * @return the next bill number
-     */
-    private int getNextBillNumber() {
-    	TableLayout contentTable = (TableLayout) getActivity().findViewById(R.id.ContentTable);
-    	TableRow lastRow = (TableRow) contentTable.getChildAt(contentTable.getChildCount()-1);
-    	int number = 0;
-    	try {
-    		EditText lastNumber = (EditText) lastRow.getChildAt(0);
-    		String text = lastNumber.getText().toString();
-    		number = Integer.parseInt(text) + 1;
-    	} catch (Exception e) {
-    		number = 1;
-    	}
-    	return number;
-    }
 
     /**
      * Updates the information with a bill information of a given row if the
@@ -156,7 +97,7 @@ public class BillRowFragment extends Fragment {
      * @param row of which the fields will be updated
      */
     private void updateRowInfo(TableRow row) {
-    	Bundle args = getArguments();
+    	/*Bundle args = getArguments();
         if (args != null) {
         	EditText t1 = (EditText) row.getChildAt(1);
         	t1.setText(Integer.toString(args.getInt(KEY_ARG_NIT)));
@@ -175,6 +116,6 @@ public class BillRowFragment extends Fragment {
         	
         	EditText t6 = (EditText) row.getChildAt(6);
         	t6.setText(args.getString(KEY_ARG_CONTROL_CODE));
-        }
+        }*/
     }
 }
