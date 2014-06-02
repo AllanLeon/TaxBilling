4f0b2fafb991450efaa192a489363717bd28097e
Allan Leon <allan.leon@outlook.com>
2014-06-02 00:18:45 -0400

    Modified and added some methods on BillRow class to implement the last changes

--- src/com/upb/taxbilling/view/BillRow.java
+++ src/com/upb/taxbilling/view/BillRow.java
@@ -3,36 +3,57 @@ package com.upb.taxbilling.view;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-import android.app.Fragment;
 import android.content.Context;
-import android.os.Bundle;
 import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TableLayout;
 import android.widget.TableRow;
 
-import com.upb.taxbilling.R;
+import com.upb.taxbilling.exceptions.BillException;
 import com.upb.taxbilling.model.data.Bill;
 
 /**
- * The fragment where a bill row is stored.
+ * The TableRow where a bill is stored.
  * @author Allan Leon
  */
 public class BillRow extends TableRow {
 
-	Bill bill;
-	
-	public BillRow(Context context) {
+	private Bill bill;
+	private int rowNumber;
+
+	/**
+	 * Constructor method that receives the context and the number of the row as
+	 * parameters.
+	 * @param context
+	 * @param rowNumber
+	 */
+	public BillRow(Context context, int rowNumber) {
 		super(context);
-		addViewsToRow(this);
+		this.rowNumber = rowNumber;
+		initializeComponents();
 	}
 
 	/**
-	 * Updates the bill attribute with a given bill.
+	 * Constructor method that receives the context, the number of the row
+	 * and a bill as parameters, then updates it's components based on the
+	 * bill it contains.
+	 * @param context
+	 * @param rowNumber
 	 * @param bill
+	 */
+	public BillRow(Context context, int rowNumber, Bill bill) {
+		super(context);
+		this.rowNumber = rowNumber;
+		this.bill = bill;
+		initializeComponents();
+		try {
+			updateRowInfo();
+		} catch (BillException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param bill the bill to set.
 	 */
 	public void setBill(Bill bill) {
 		this.bill = bill;
@@ -44,78 +65,90 @@ public class BillRow extends TableRow {
 	public Bill getBill() {
 		return bill;
 	}
+	
+	/**
+	 * @param rowNumber the rowNumber to be set.
+	 */
+	public void setRowNumber(int rowNumber) {
+		this.rowNumber = rowNumber;
+	}
+	
+	/**
+	 * @return returns this row's number.
+	 */
+	public int getRowNumber() {
+		return rowNumber;
+	}
 
 	/**
-	 * Adds seven EditText fields in a given row and updates the info of them.
-	 * @param row in which the fields will be added
+	 * Initializes the seven EditText fields in this row.
 	 */
-	private void addViewsToRow(TableRow row) {
-		EditText t1 = new EditText(row.getContext());
+	private void initializeComponents() {
+		EditText t1 = new EditText(this.getContext());
     	t1.setInputType(InputType.TYPE_CLASS_NUMBER);
-    	//t1.setText(Integer.toString(getNextBillNumber()));
-    	t1.setText("0");
+    	t1.setText(Integer.toString(rowNumber));
     	t1.setEnabled(false);
     	
-    	EditText t2 = new EditText(row.getContext());
+    	EditText t2 = new EditText(this.getContext());
     	t2.setInputType(InputType.TYPE_CLASS_NUMBER);
     	t2.setText("");
     	
-    	EditText t3 = new EditText(row.getContext());
+    	EditText t3 = new EditText(this.getContext());
     	t3.setInputType(InputType.TYPE_CLASS_NUMBER);
     	t3.setText("");
     	
-    	EditText t4 = new EditText(row.getContext());
+    	EditText t4 = new EditText(this.getContext());
     	t4.setInputType(InputType.TYPE_CLASS_NUMBER);
     	t4.setText("");
     	
-    	EditText t5 = new EditText(row.getContext());
+    	EditText t5 = new EditText(this.getContext());
     	t5.setInputType(InputType.TYPE_CLASS_DATETIME);
     	t5.setText("");
     	
-    	EditText t6 = new EditText(row.getContext());
+    	EditText t6 = new EditText(this.getContext());
     	t6.setInputType(InputType.TYPE_CLASS_NUMBER);
     	t6.setText("");
     	
-    	EditText t7 = new EditText(row.getContext());
+    	EditText t7 = new EditText(this.getContext());
     	t7.setInputType(InputType.TYPE_CLASS_NUMBER);
     	t7.setText("");
     	
-    	row.addView(t1);
-    	row.addView(t2);
-    	row.addView(t3);
-    	row.addView(t4);
-    	row.addView(t5);
-    	row.addView(t6);
-    	row.addView(t7);
-    	
-    	updateRowInfo(row);
+    	this.addView(t1);
+    	this.addView(t2);
+    	this.addView(t3);
+    	this.addView(t4);
+    	this.addView(t5);
+    	this.addView(t6);
+    	this.addView(t7);
 	}
 
     /**
-     * Updates the information with a bill information of a given row if the
-     * arguments aren't empty.
-     * @param row of which the fields will be updated
+     * Updates the editTexts fields of this row with the information of the bill
+     * it contains.
+     * @throws BillException when the bill is null
      */
-    private void updateRowInfo(TableRow row) {
-    	/*Bundle args = getArguments();
-        if (args != null) {
-        	EditText t1 = (EditText) row.getChildAt(1);
-        	t1.setText(Integer.toString(args.getInt(KEY_ARG_NIT)));
+    public void updateRowInfo() throws BillException {
+        if (bill != null) {
+        	DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         	
-        	EditText t2 = (EditText) row.getChildAt(2);
-        	t2.setText(Integer.toString(args.getInt(KEY_ARG_BILL_NUMBER)));
+        	EditText t1 = (EditText) this.getChildAt(1);
+        	t1.setText(Integer.toString(bill.getNit()));
         	
-        	EditText t3 = (EditText) row.getChildAt(3);
-        	t3.setText(Integer.toString(args.getInt(KEY_ARG_AUTORIZATION_NUMBER)));
+        	EditText t2 = (EditText) this.getChildAt(2);
+        	t2.setText(Integer.toString(bill.getBillNumber()));
         	
-        	EditText t4 = (EditText) row.getChildAt(4);
-        	t4.setText(args.getString(KEY_ARG_DATE));
+        	EditText t3 = (EditText) this.getChildAt(3);
+        	t3.setText(Integer.toString(bill.getAutorizationNumber()));
         	
-        	EditText t5 = (EditText) row.getChildAt(5);
-        	t5.setText(Double.toString(args.getInt(KEY_ARG_AMOUNT)));
+        	EditText t4 = (EditText) this.getChildAt(4);
+        	t4.setText(df.format(bill.getEmissionDate()));
         	
-        	EditText t6 = (EditText) row.getChildAt(6);
-        	t6.setText(args.getString(KEY_ARG_CONTROL_CODE));
-        }*/
+        	EditText t5 = (EditText) this.getChildAt(5);
+        	t5.setText(Double.toString(bill.getAmount()));
+        	
+        	EditText t6 = (EditText) this.getChildAt(6);
+        	t6.setText(bill.getControlCode());
+        }
+        throw new BillException("The bill doesn't contain information.");
     }
 }
