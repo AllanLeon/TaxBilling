dfdc0dd8f1ff8562300b5c91b90ecd7fdecf0d44
Allan Leon <allan.leon@outlook.com>
2014-06-08 21:53:11 -0400

    Updated tokenize method into a simpler one

--- src/com/upb/taxbilling/controller/billanalyzer/BillAnalyzer.java
+++ src/com/upb/taxbilling/controller/billanalyzer/BillAnalyzer.java
@@ -1,9 +1,7 @@
 package com.upb.taxbilling.controller.billanalyzer;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class BillAnalyzer {
 	
@@ -13,15 +11,13 @@ public class BillAnalyzer {
 	}
 	
 	/**
-	 * Divides a given string into tokens using the bill pattern and then returns a list of
-	 * the tokens obtained.
+	 * Divides a given string into tokens using the divider of this class
+	 * and then returns a list of the tokens obtained.
 	 * @param billText string to be divided in tokens
 	 * @return a list of the tokens obtained
 	 */
 	public List<String> tokenizeBillText(String billText) {
-		List<String> billInfo = new ArrayList<String>();
-		while(!billText.equals("")) {
-		}
+		List<String> billInfo = Arrays.asList(billText.split(DIVIDER));
 		return billInfo;
 	}
 }
