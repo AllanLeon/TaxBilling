ea43e64e08179c765c6a1e7f100bb6b69ff4d797
Allan Leon <allan.leon@outlook.com>
2014-06-08 22:30:07 -0400

    Created createManualBill method

--- src/com/upb/taxbilling/controller/billanalyzer/BillAnalyzer.java
+++ src/com/upb/taxbilling/controller/billanalyzer/BillAnalyzer.java
@@ -1,7 +1,13 @@
 package com.upb.taxbilling.controller.billanalyzer;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import com.upb.taxbilling.exceptions.BillException;
+import com.upb.taxbilling.model.data.Bill;
 
 public class BillAnalyzer {
 	
@@ -19,5 +25,35 @@ public class BillAnalyzer {
 	public List<String> tokenizeBillText(String billText) {
 		List<String> billInfo = Arrays.asList(billText.split(DIVIDER));
 		return billInfo;
+	}
+	
+	/**
+	 * Creates a manual bill with a given list of a bill's information.
+	 * @param billInfo information of the bill
+	 * @return a new manual bull
+	 * @throws BillException if the bill couldn't be created
+	 */
+	private Bill createManualBill(List<String> billInfo) throws BillException {
+		try {
+			int nit = Integer.parseInt(billInfo.get(0));
+			String name = billInfo.get(1);
+			int autorizationNumber = Integer.parseInt(billInfo.get(2));
+			Date limitEmissionDate = new SimpleDateFormat(
+					"dd/MM/yyyy", Locale.ENGLISH).parse(billInfo.get(3));
+			int aux = 4;
+			Double ammount = 0.0;
+			if (billInfo.size() == 7) {
+				ammount = Double.parseDouble(billInfo.get(aux));
+				aux++;
+			}
+			int economicActivity = Integer.parseInt(billInfo.get(aux));
+			int subsidiary = Integer.parseInt(billInfo.get(aux + 1));
+			
+			return new Bill(nit, name, autorizationNumber, limitEmissionDate,
+					ammount, economicActivity, subsidiary);
+		} catch (Exception ex) {
+			throw new BillException("El formato de la factura manual es incorrecto," +
+					"el problema puede ser debido a:\n" + ex.getMessage());
+		}
 	}
 }
