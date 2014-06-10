4ee350d7b2b82378349e1184c405846167af2983
= <=>
2014-06-10 00:17:13 -0400

    Add Email Register and Export

--- src/com/upb/taxbilling/model/data/Taxpayer.java
+++ src/com/upb/taxbilling/model/data/Taxpayer.java
@@ -15,17 +15,19 @@ public class Taxpayer {
 	private String NameLastname;
 	private String Address;
 	private String ExpeditionPlace;
+	private String Email;
 	private int IdentityNumber;
 
 	/**
 	 * Constructor method with parameters
 	 */
 	 
-	public Taxpayer(String NameLastname, String Address, String ExpeditionPlace, int IdentityNumber)
+	public Taxpayer(String NameLastname, String Address, String ExpeditionPlace, String Email, int IdentityNumber)
 	{
 		this.NameLastname = NameLastname;
 		this.Address = Address;
 		this.ExpeditionPlace = ExpeditionPlace;
+		this.Email = Email;
 		this.IdentityNumber = IdentityNumber;
 	}
 	
@@ -49,7 +51,12 @@ public class Taxpayer {
 	public int getIdentityNumber() {
 		return IdentityNumber;
 	}
-
+	
+	public String getEmail() 
+	{
+		return Email;
+	}
+	
 	public void setNameLastname(String nameLastname) {
 		NameLastname = nameLastname;
 	}
@@ -65,5 +72,10 @@ public class Taxpayer {
 	public void setIdentityNumber(int identityNumber) {
 		IdentityNumber = identityNumber;
 	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
 	
 }
