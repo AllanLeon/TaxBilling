422cae32b560b0515b691fe3df8d07273f442c97
Allan Leon <allan.leon@outlook.com>
2014-06-19 12:15:30 -0400

    Changed row color

--- src/com/upb/taxbilling/MainMenu.java
+++ src/com/upb/taxbilling/MainMenu.java
@@ -11,9 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.upb.taxbilling.view.ExportBill;
 import com.upb.taxbilling.view.RegisterFragment;
